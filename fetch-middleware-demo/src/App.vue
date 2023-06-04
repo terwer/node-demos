23123b44a9220484ce04d2c274488d816ba3611f
terwer <youweics@163.com>
2023-06-04 19:20:50 +0800

    feat: add middleware fetch test

--- fetch-middleware-demo/src/App.vue
+++ fetch-middleware-demo/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import MiddlewareFetchTest from "./components/MiddlewareFetchTest.vue"
 </script>
 
 <template>
@@ -11,9 +11,9 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <middleware-fetch-test />
 </template>
-
+Ø
 <style scoped>
 .logo {
   height: 6em;
---
+++ fetch-middleware-demo/src/components/MiddlewareFetchTest.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>MiddlewareFetchTest</div>
+</template>
+
+<style scoped></style>
